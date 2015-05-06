e6dfaa6d8976ba1aac3ef91726f3e6cc0d30f138
Melissa Johnson <melj@stanford.edu>
2015-05-05 23:06:34 -0700

    added super basic email form

--- public/javascripts/src/email.jsx
+++ public/javascripts/src/email.jsx
@@ -4,11 +4,33 @@ module.exports = React.createClass({
 doAlert: function() {
     alert("hi there!");
 },
+
+formSubmitted : function() {
+    event.preventDefault(event);
+    //SEND EMAIL TO SERVER HERE
+    console.log(this.state.emailAddress);
+    this.props.onNextButtonClicked()
+},
+
+emailChanged : function(event) {
+ this.setState ({
+       emailAddress  : event.target.value
+    });
+    //validate email here?
+},
+
 render: function() {
     return (
     <div>
         <h1>This is another email component again</h1>
-        <input type='button' value='next' onClick={this.props.onNextButtonClicked} />
+         <form onSubmit={this.formSubmitted}>
+            <input type='text' placeholder='email' name='email_address'
+                //value={this.state.name}
+                ref='email'
+                onChange={this.emailChanged}
+                autoFocus={true} />
+        <input type='submit' value='next' onClick={this.formSubmitted} />
+        </form>
     </div>
     )
 }
