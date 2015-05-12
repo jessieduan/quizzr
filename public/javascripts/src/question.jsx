b7dda276869c9e661ec574851eadae39203f4a22
magsko <Maggie@DN0a2269d9.SUNet>
2015-05-12 00:43:18 -0700

    addin explanationBox

--- public/javascripts/src/question.jsx
+++ public/javascripts/src/question.jsx
@@ -1,9 +1,45 @@
 var React = require('react');
+var ExplanationBoxComponent = require('./explanationBox.jsx');
+
+var explanations = 
+[
+  { "explanation" : "This is an explanation 1"}, 
+  { "explanation" : "This is an explanation 2"}
+];
 
 module.exports = React.createClass({
+
+
+getInitialState: function() {
+
+	return {explanations: []}
+},
+
+componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(explanations) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
 render: function() {
     return (
-    <h1>This is a question component</h1>
+   <div>
+	    <h1>This is a question number question number {this.props.questionNum} </h1>
+	    <input type='button' value='next' onClick={this.props.onNextButtonClicked} />
+	    
+	   			<ExplanationBoxComponent explanations={explanations} />
+	   			 
+	   			 
+
+	</div>    
     )
 }
 });
