3893e6ef639fbe9b127f10c39d5ba47908b83bae
Melissa Johnson <melj@stanford.edu>
2015-05-12 00:35:40 -0700

    quiz is ready to be hooked up to db

--- public/javascripts/src/question.jsx
+++ public/javascripts/src/question.jsx
@@ -2,24 +2,60 @@ var React = require('react');
 
 module.exports = React.createClass({
 
+    getInitialState : function() {
+        return {
+            selectedQuestion : undefined,
+        };
+    },
+
+    selectAnswer : function(event) {
+        //console.log(this.props.)
+        /*this.setState ({
+            selectedQuestion : questionNum,
+        });*/
+
+        //alert(questionNum);
+        //quiz id
+        //question id
+        //answer chosen = answer_id
+        //user id
+        //alert("selected question")
+         //var selectedAnswer = this.refs.answersGroup.getCheckedValue();
+        var index = event.target.value;
+        console.log(index);
+        this.props.onAnswerSelected(this.props.question["answers"][index]);
+    },
+
 getAnswers : function() {
+//                 value={answers[i]['answer_id']}
     var listItems = [];
     var answers = this.props.question["answers"];
     for (var i = 0; i < answers.length; i++) {
-        var newAnswer = (<h1>{answers[i]["answer"]}</h1>);
+        var newAnswer = (
+            <label>
+                <input type="radio"
+                 name="answerButtons"
+                 value={i}
+                 onClick={this.selectAnswer} />
+                    {answers[i]["answer"]}
+            </label>);
         listItems.push(newAnswer);
     }
     return listItems;
 },
 
 render: function() {
+    console.log("received data: ");
+    console.log(this.props.question)
     return (
-    <div>
+        <div>
         <h1>This is question number {this.props.questionNum} </h1>
         <h2>{this.props.question["question_str"]}</h2>
-        {this.getAnswers()}
+        <form onSubmit={this.formSubmitted}>
+                <div> {this.getAnswers()} </div>
+        </form>
         <input type='button' value='next' onClick={this.props.onNextButtonClicked} />
-    </div>
-    )
+        </div>
+        )
 }
 });
