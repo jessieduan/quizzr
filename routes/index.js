903ac8facaecea0efdd6fad35f2483701b5868a9
Jessie Duan <jduan48@gmail.com>
2015-05-12 23:41:31 -0700

    update database when answer is submitted

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,75 @@ router.get('/newuser', function(req, res) {
     res.render('newuser', { title: 'Add New User' });
 });
 
-/* POST to Add User Service */
+/* POST when question answer is submitted */
+router.post('/submitanswer', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userEmail = req.body.useremail;
+    var quizID = req.body.quizID;
+    var questionID = req.body.questionID;
+    var answerID = req.body.answerID;
+    var explanationWritten = req.body.explanationWritten; // or none exists
+    var explanationVotedFor = req.body.explanationVotedFor; // or none exists
+    var wasUpvote = req.body.wasUpvote; // or none exists
+
+    var explanationWrittenID = -1;
+
+    var quiz = db.quizzes.findOne({ quiz_id: quizID });
+    var isCorrect = (answerID == quiz["" + questionID]["correct_answer_id"]);
+
+    // HACKS ON HACKS ON HACKS - can't use $push, so we resort to manually updating collections instead
+    // add explanation
+    if (explanationWritten) {
+        db.quizzes.find({ quiz_id: quizID }).forEach(function (doc) {
+            var explanations = doc["questions"]["" + questionID]["answers"]["" + answerID]["explanations"];
+            explanationWrittenID = explanations.length + 1;
+            explanations.push({
+                "explanation_id": explanationWrittenID,
+                "explanation": explanationWritten,
+                "upvotes": 0
+            });
+            db.quizzes.save(doc, function (err, doc) {
+                if (err) res.send("There was a problem adding the information to the database.");
+            });
+        });
+    }
+
+    // update upvotes for explanation
+    if (explanationVotedFor) {
+        // TODO
+    }
+
+    // update users with user attempt
+    db.users.find({ email: userEmail }).forEach(function (user) {
+        user["quizzes_taken"]["" + quizID]["attempts"].push({
+            "question_id": questionID,
+            "is_correct": isCorrect,
+            "answer_given": answerID,
+            "upvoted_explanation": explanationVotedFor,
+            "wrote_explanation": explanationWrittenID
+        });
+
+        // HACK - everyone will get to this point if there's no previous error, so it's ok to alter state based on success of this one call.
+        db.users.save(user, function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            } else {
+                // If it worked, set the header so the address bar doesn't still say /adduser
+                // res.location("mongotest");
+                // And forward to success page
+                // res.redirect("mongotest");
+            }
+        });
+    });
+
+});
+
+/* POST to Add User Service - TEST */
 router.post('/adduser', function(req, res) {
 
     // Set our internal DB variable
